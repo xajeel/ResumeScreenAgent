f72e2c6c32b4a062414e5b8ef96463ae46d3446f
sajeel <sajeel135@gmail.com>
2025-04-26 23:48:37 +0500

    data extraction model

--- src/model.py
+++ src/model.py
@@ -1,8 +1,17 @@
-from google.generativeai import genai 
+import google.generativeai as genai
 from dotenv import load_dotenv
 import os
+import re
+
+from utils.prompt import model_prompt
 
 load_dotenv()
 api_key = os.getenv("GOOGLE_API_KEY")
 
-genai.configure(api_key=api_key)
+def extract_candidate_info(job_description, cv_data):
+    genai.configure(api_key=api_key)
+    prompt = model_prompt(job_description, cv_data)
+    model = genai.GenerativeModel("gemini-2.0-flash")
+    response = model.generate_content(prompt)
+    clean_response = re.sub(r"```json|```", "", response.text).strip()
+    return clean_response
